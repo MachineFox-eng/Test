b2b828826504585f3c1686a28189a1e516792a28
MachineFox <eduardokaminski05@gmail.com>
2025-03-07 16:06:23 -0300

    Update message.lua

--- message.lua
+++ message.lua
@@ -207,7 +207,7 @@ local accessorylimbs={
 	{meshid="14768666349",textureid="",C0=cf_0,Name="Torso"},
 	{meshid="14241018198",textureid="",C0=cf_0,Name="Torso"},
 	{meshid="13421774668",textureid="",C0=cf_0,Name="Torso"},
-	{meshid="111946216585470",textureid="",C0=cf_0,Name="Torso"},
+	{meshid="91981815806560",textureid="",C0=cf_0,Name="Torso"},
 	{meshid="127552124837034",textureid="",C0=angles(0,0,1.5707963267948966),Name="Torso"},
 
 	{meshid="4324138105",textureid="4324138210",C0=angles(-1.6144295580947547,1.5707963267948966,0)*cf(-0.125,0.3,0),Name="Left Arm"},
